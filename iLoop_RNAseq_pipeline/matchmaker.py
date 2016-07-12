66a95d2da59a04175ef8039b81cfafd9445ec8c6
meono <emoz@biosustain.dtu.dk>
2016-07-12 13:38:49 +0200

    Various changes to appropriate the modules to our system and purposes. Several functions are changes, more are ignored for now. This is only a test commit.

---
+++ iLoop_RNAseq_pipeline/matchmaker.py
@@ -0,0 +1,147 @@
+from __future__ import division, print_function
+import collections
+import logging
+
+
+def check_read_uniqueness(reads):
+    read_files = [read for read_list in reads.values() for read in read_list]
+    if len(read_files) == len(set(read_files)):
+        return True
+    else:
+        report = {}
+        duplicates = [read for read, count in collections.Counter(read_files).items() if count > 1]
+        for duplicate in duplicates:
+            for path, read_list in reads.items():
+                if duplicate in read_list:
+                    try:
+                        report[duplicate].add(path)
+                    except:
+                        report[duplicate] = {path}
+        logging.warning('There are reads with identical names under different paths:\n')
+        for read, paths in report.items():
+            logging.warning('{} found under'.format(read))
+            for path in paths:
+                logging.warning('\t{}'.format(path))
+        logging.error('Check input reads and restart.')
+        return False
+
+
+def lcs(file1, file2):
+    # find longest common substring
+    # based on https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring
+    m = [[0] * (1 + len(file2)) for i in range(1 + len(file1))]
+    longest, x_longest = 0, 0
+    for x in range(1, 1 + len(file1)):
+        for y in range(1, 1 + len(file2)):
+            if file1[x - 1] == file2[y - 1]:
+                m[x][y] = m[x - 1][y - 1] + 1
+                if m[x][y] > longest:
+                    longest = m[x][y]
+                    x_longest = x
+            else:
+                m[x][y] = 0
+    return file1[x_longest - longest: x_longest]
+
+
+def find_base(read_file, read_files):
+    # split the sample name from illumina tags
+    replicate = '_'.join(read_file.split('_')[:-4])
+
+    longest = 0
+    rep = ''
+    for read in read_files:
+        tmp = lcs(replicate, read)
+        if len(tmp) > longest:
+            longest = len(tmp)
+            rep = tmp
+    return rep.strip('_')
+
+def print_experiment(groups):
+    pstr = ''
+    for group, replicates in groups.items():
+        pstr += group + '\n'
+        for replicate, reads in replicates.items():
+            pstr += '\t' + replicate + '\n'
+            reads.sort()
+            for read in reads:
+                pstr += '\t\t' + read + '\n'
+    return pstr
+
+def find_groups(reads):
+
+    # check if there are reads with the same name under different folders
+    if not check_read_uniqueness(reads):
+        return False
+
+    data_files = {read: dirName for dirName, read_files in reads.items() for read in read_files}
+
+    groups = dict()
+    found = set()
+    for read, dirName in data_files.items():
+        replicate = find_base(read, data_files.keys())
+        basename = find_base(read,[data_file for data_file in data_files.keys() if not data_file.startswith(replicate)])
+        found.add(basename)
+        if not groups.get(basename):
+            groups[basename] = {replicate: ['/'.join([dirName, read])]}
+        elif groups.get(basename) and not groups[basename].get(replicate):
+            groups[basename][replicate] = ['/'.join([dirName, read])]
+        elif groups.get(basename) and groups[basename].get(replicate):
+            groups[basename][replicate].append('/'.join([dirName, read]))
+
+    logging.info('Experiment setup:\n{}'.format(print_experiment(groups)))
+    return groups
+
+
+
+
+    #     pairs = []
+    #     full_path = []
+    #     easy_read = []
+    #
+    #     for forward_file in glob.glob(os.path.join(my_input, '*_1.fastq.gz')):
+    #         forward_path, forward_name = forward_file.rsplit("/", 1)
+    #         sample_id, ext = forward_name.rsplit("_", 1)
+    #         reverse_name = sample_id + '_2.fastq.gz'
+    #
+    #         if os.path.isfile(reverse_name) is True:
+    #             pairs.append((forward_name, reverse_name))
+    #             full_path.append((forward_file, os.path.join(forward_path+"/"+reverse_name)))
+    #
+    #     for index, item in enumerate(pairs, start=1):
+    #         easy_read.append((index, item))
+    #
+    #     table = tabulate(easy_read, headers=["#", "Pair"], tablefmt="grid")
+    #     print (table)
+    #
+    #     mates_q = input("Are these pairings correct? ([y]/n) ")
+    #     if mates_q.lower() != "y":
+    #         print("Run aborted. Double check file names.")
+    #         return
+    #     else:
+    #         print ("Ok saving table to Matchmaker folder")
+    #         filename = os.path.join(output_path, 'table_pairs.txt')
+    #         f = open(filename, 'w')
+    #         f.write(table)
+    #         f.close()
+    #
+    #     # correct delimiters for the cluster
+    #     data_files = []
+    #     for i in range(len(full_path)):
+    #         data_files.append(str.join(' ', full_path[i]))
+    #
+    # else:
+    #     print("Run aborted.")
+    #     return
+    #
+    # task_count = len(data_files)
+    # assert task_count > 0, "Could not find any fastq files in folder %s" % my_input
+    #
+    #
+    # list_index=os.path.join(output_path,'read_files.txt')
+    # f=open(list_index,'w')
+    # f.write(str(data_files))
+    # f.close()
+    # print ("All done! Files are located in %s" %output_path)
+
+# get path and run!
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+__author__ = 'emre'
+
+from setuptools import setup
+
+requirements = ['collections>=',
+                'validate_email>=1.3']
+
+setup(
+    name='iLoop_RNAseq_pipeline',
+    version='0.1dev',
+    packages=['iLoop_RNAseq_pipeline',],
+    license='Apache License, Version 2.0',
+    include_package_data=True,
+    # data_files=[('iLoop_RNAseq_pipeline', ['iLoop_RNAseq_pipeline/defaults/References.tsv', 'iLoop_RNAseq_pipeline/defaults/RNAseq_pipeline_defaults.txt'])],
+    # long_description=open('dontREADMEyet.md').read(),
+)
