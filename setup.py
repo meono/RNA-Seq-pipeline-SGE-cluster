485a78172ea394f725ab06cf328fe9cb536f1619
meono <emoz@biosustain.dtu.dk>
2016-07-14 10:44:49 +0200

    fix: run_RNAseq.py needs to be executable

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 __author__ = 'emre'
 
+import os
+import stat
 from setuptools import setup
+
+# run_RNAseq file needs to be executable but git won't set that properly
+os.chmod(os.path.join(os.getcwd(), 'iLoop_RNAseq_pipeline', 'run_RNAseq.py'),
+         os.stat(os.path.join(os.getcwd(), 'iLoop_RNAseq_pipeline',
+                              'run_RNAseq.py')).st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 
 requirements = ['validate_email>=1.3',
                 'pandas>=0.18.1']
@@ -8,7 +15,7 @@ requirements = ['validate_email>=1.3',
 setup(
     name='iLoop_RNAseq_pipeline',
     version='0.1dev',
-    packages=['iLoop_RNAseq_pipeline',],
+    packages=['iLoop_RNAseq_pipeline'],
     license='Apache License, Version 2.0',
     include_package_data=True,
     install_requires=requirements,
